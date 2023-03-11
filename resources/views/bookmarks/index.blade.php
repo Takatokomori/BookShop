05ead7aa75c87eb747d2c538a6ebbedc0a2cc0ef
editor-taka <takachan1050150@gmail.com>
2023-03-11 17:09:13 +0900

    installed ide-helper

--- resources/views/bookmarks/index.blade.php
+++ resources/views/bookmarks/index.blade.php
@@ -1,21 +1,23 @@
 <x-app-layout>
-    <table class="table table-striped">
-    <thead>
-        <tr>
-        <th>ブックナンバー</th>
-        <th>ブック名</th>
-        <th>作成日</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach ($bookmarks as $book)
-        <tr>
-        <td>{{ $book->id }}</td>
-        <td>{{ $book->title }}</td>
-        <td>{{ $book->created_at }}</td>
-        </tr>
-        @endforeach
-    </tbody>
-    </table>
-    {{ $bookmarks->links()}}
+    <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
+        <table class="table table-striped">
+        <thead>
+            <tr>
+            <th>ブックナンバー</th>
+            <th>ブック名</th>
+            <th>作成日</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($bookmarks as $book)
+            <tr>
+            <td>{{ $book->id }}</td>
+            <td>{{ $book->title }}</td>
+            <td>{{ $book->created_at }}</td>
+            </tr>
+            @endforeach
+        </tbody>
+        </table>
+        {{ $bookmarks->links()}}
+    </div>
 </x-app-layout>
